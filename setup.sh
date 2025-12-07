81bd222451c462a970058792de5b9b358968d63d
Emre-2R <emreocakoglu3@gmail.com>
2025-12-07 03:47:44 +0300

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install build-essential meson ninja-build pkg-config \
+libglib2.0-dev libcairo2-dev libpango1.0-dev libx11-dev \
+libx11-xcb-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev \
+libxcb-keysyms1-dev libxcb-randr0-dev libxcb-xinerama0-dev \
+libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev libstartup-notification0-dev \
+libgdk-pixbuf2.0-dev
